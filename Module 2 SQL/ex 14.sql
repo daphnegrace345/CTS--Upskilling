b0ff1b0e498384253c01db9917abb3b55c0e80db
daphnegrace345 <daphnegrace345@gmail.com>
2026-06-21 12:53:18 +0530

    Create ex 14.sql

---
+++ Module 2 SQL/ex 14.sql
@@ -0,0 +1,7 @@
+-- 14. Most Registered Events
+SELECT e.title, COUNT(r.user_id) AS registrations
+FROM Events e
+JOIN Registrations r ON e.event_id = r.event_id
+GROUP BY e.event_id
+ORDER BY registrations DESC
+LIMIT 3;
