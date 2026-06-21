ed384db244c001526a25053c72613b636b26bffd
daphnegrace345 <daphnegrace345@gmail.com>
2026-06-21 13:05:39 +0530

    first commit

---
+++ Module 2 SQL/ex 4.sql
@@ -0,0 +1,6 @@
+-- 4. Peak Session Hours
+SELECT e.title, COUNT(*) AS session_count
+FROM Sessions s
+JOIN Events e ON s.event_id = e.event_id
+WHERE TIME(s.start_time) BETWEEN '10:00:00' AND '11:59:59'
+GROUP BY e.event_id;
