9d1f54a3160364dc23631dc04731c91d2e24174e
daphnegrace345 <daphnegrace345@gmail.com>
2026-06-22 17:14:31 +0530

    first commit

---
+++ Module 3 Java/ex 9.java
@@ -0,0 +1,29 @@
+// Grade Calculator
+import java.util.Scanner;
+
+class GradeCalculator {
+    public static void main(String[] args) {
+
+        try (Scanner sc = new Scanner(System.in)) {
+
+            System.out.print("Enter marks out of 100: ");
+            int marks = sc.nextInt();
+
+            char grade;
+
+            if (marks >= 90 && marks <= 100) {
+                grade = 'A';
+            } else if (marks >= 80) {
+                grade = 'B';
+            } else if (marks >= 70) {
+                grade = 'C';
+            } else if (marks >= 60) {
+                grade = 'D';
+            } else {
+                grade = 'F';
+            }
+
+            System.out.println("Assigned Grade: " + grade);
+        }
+    }
+}
