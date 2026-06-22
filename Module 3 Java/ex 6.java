462537b986dce05c50d8fc33871369dadb7e70a3
daphnegrace345 <daphnegrace345@gmail.com>
2026-06-22 17:12:41 +0530

    first commit

---
+++ Module 3 Java/ex 6.java
@@ -0,0 +1,19 @@
+// Data Type Demonstration
+class DataTypesDemo {
+    public static void main(String[] args) {
+
+        // Declaring variables
+        int number = 100;
+        float price = 99.5f;
+        double salary = 45678.90;
+        char grade = 'A';
+        boolean isJavaEasy = true;
+
+        // Displaying values
+        System.out.println("Integer value: " + number);
+        System.out.println("Float value: " + price);
+        System.out.println("Double value: " + salary);
+        System.out.println("Character value: " + grade);
+        System.out.println("Boolean value: " + isJavaEasy);
+    }
+}
