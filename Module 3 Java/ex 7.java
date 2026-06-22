f3a71aa06fcb208aa24957ae060fbfa699bec4b8
daphnegrace345 <daphnegrace345@gmail.com>
2026-06-22 17:13:18 +0530

    first commit

---
+++ Module 3 Java/ex 7.java
@@ -0,0 +1,20 @@
+// Type Casting Example
+class TypeCasting {
+    public static void main(String[] args) {
+
+        // Double to int
+        double decimalValue = 45.78;
+        int intValue = (int) decimalValue;
+
+        // Int to double
+        int number = 25;
+        double doubleValue = (double) number;
+
+        // Displaying results
+        System.out.println("Original double value: " + decimalValue);
+        System.out.println("After casting to int: " + intValue);
+
+        System.out.println("Original int value: " + number);
+        System.out.println("After casting to double: " + doubleValue);
+    }
+}
