ac1499a29874b531d8f6441cadbb0ed18f5dd676
daphnegrace345 <daphnegrace345@gmail.com>
2026-06-22 17:07:50 +0530

    first commit

---
+++ Module 3 Java/ex 38.java
@@ -0,0 +1,16 @@
+// Decompile a Class File
+
+class DecompileDemo {
+
+    void display() {
+
+        System.out.println("Java Decompilation Example");
+    }
+
+    public static void main(String[] args) {
+
+        DecompileDemo obj = new DecompileDemo();
+
+        obj.display();
+    }
+}
