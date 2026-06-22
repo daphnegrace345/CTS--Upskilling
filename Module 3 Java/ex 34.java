86a0ad893d861e125d5d900ff5733a3695a5b515
daphnegrace345 <daphnegrace345@gmail.com>
2026-06-22 17:04:56 +0530

    first commit

---
+++ Module 3 Java/ex 34.java
@@ -0,0 +1,16 @@
+// Create and Use Java Modules
+
+class Utility {
+
+    static void showMessage() {
+
+        System.out.println("Welcome to Java Modules");
+    }
+}
+
+class Greetings {
+    public static void main(String[] args) {
+
+        Utility.showMessage();
+    }
+}
